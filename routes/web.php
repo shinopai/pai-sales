303fd3df82a0d5b4cff2d6ae10d228b633b2bc7a
shinopai <harbor.shinozaki@gmail.com>
2023-08-09 16:20:18 +0900

    新規登録＆ログイン後の遷移先変更

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* root */
+// root
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // 売上管理
+    Route::prefix('performances')->name('performances.')->group(function () {
+        // 一覧画面
+        Route::get('/', [App\Http\Controllers\Performance\IndexController::class, 'index'])->name('index');
+    });
 });
 
 require __DIR__.'/auth.php';
